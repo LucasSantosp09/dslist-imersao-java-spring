1e9201dcb99dcd4794e049372a41519cbef5d581
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-05-10 21:26:14 -0300

    Get List

--- src/main/java/com/devsupeior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsupeior/dslist/controllers/GameListController.java
@@ -9,27 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsupeior.dslist.dto.GameDTO;
+import com.devsupeior.dslist.dto.GameListDTO;
 import com.devsupeior.dslist.dto.GameMinDTO;
+import com.devsupeior.dslist.services.GameListService;
 import com.devsupeior.dslist.services.GameService;
 
 @RestController
-@RequestMapping(value = "/games")
-public class GameController {
+@RequestMapping(value = "/lists")
+public class GameListController {
 	
 	@Autowired
-	private GameService gameService;
+	private GameListService gameListService;
 	
-	@GetMapping(value = "/{id}")
-	public GameDTO findById(@PathVariable Long id) {
-		GameDTO result = gameService.findById(id);
-		return result;
 
-	}
-	
 	
 	@GetMapping
-	public List<GameMinDTO> findAll() {
-		List<GameMinDTO> result = gameService.findAll();
+	public List<GameListDTO> findAll() {
+		List<GameListDTO> result = gameListService.findAll();
 		return result;
 	}
 }
---
+++ src/main/java/com/devsupeior/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.devsupeior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsupeior.dslist.dto.GameListDTO;
+import com.devsupeior.dslist.entities.GameList;
+import com.devsupeior.dslist.repository.GameListRepository;
+
+@Service
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(GameListDTO::new).toList();
+	}
+	
+	
+}
